7f5d61f358e985c225b83ada02e908f3e30a490d
juanreina <juandavidreinams@gmai.com>
2024-10-15 15:32:44 -0500

    database

--- movies_crud/settings.py
+++ movies_crud/settings.py
@@ -90,11 +90,11 @@ WSGI_APPLICATION = 'movies_crud.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': env('DB_NAME'),
-        'USER': env('DB_USER'),
-        'PASSWORD': env('DB_PASSWORD'),
-        'HOST': env('DB_HOST'),
-        'PORT': env('DB_PORT', default='3306'),
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT'),
     }
 }
 
