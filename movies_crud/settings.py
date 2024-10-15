9ca34f86a9718c635bbd196af5d0304c85dd2226
juanreina <juandavidreinams@gmai.com>
2024-10-15 18:31:39 -0500

    debug-db

--- movies_crud/settings.py
+++ movies_crud/settings.py
@@ -98,6 +98,13 @@ DATABASES = {
     }
 }
 
+print("DB_NAME:", os.environ.get('DB_NAME'))
+print("DB_USER:", os.environ.get('DB_USER'))
+print("DB_PASSWORD:", os.environ.get('DB_PASSWORD'))
+print("DB_HOST:", os.environ.get('DB_HOST'))
+print("DB_PORT:", os.environ.get('DB_PORT'))
+
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
