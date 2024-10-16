6e7b25f543dad0be88cce1baaef53df4dd44f331
juanreina <juandavidreinams@gmai.com>
2024-10-15 20:11:44 -0500

    db

--- movies_crud/settings.py
+++ movies_crud/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY', default='adaksdpkasdpadkwo019ix')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = 'RENDER' not in os.environ
