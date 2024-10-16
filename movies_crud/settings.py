acc79d9314af7c76de8feeb28871d86126622fee
juanreina <juandavidreinams@gmai.com>
2024-10-15 21:23:36 -0500

    media

--- movies_crud/settings.py
+++ movies_crud/settings.py
@@ -128,8 +128,8 @@ if not DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/img/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'img')
 
 
 LOGIN_URL = '/login'
